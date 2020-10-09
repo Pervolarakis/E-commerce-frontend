c23047ec40904a5220ea425f8476c3237bb477c1
Your Name <mihalaras11@gmail.com>
2020-10-09 14:39:38 +0300

    added bootstrap to sidebar

--- src/Containers/Body/SideBar.js
+++ src/Containers/Body/SideBar.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import {ListGroup} from 'react-bootstrap'
 import {NavLink} from 'react-router-dom'
 import * as colors from '../../Colors'
 
@@ -8,36 +9,34 @@ class SideBar extends Component{
 
     itemStyle={
         backgroundColor: "white",
-        border: "2px solid black",
         color: colors.color1
     }
 
     activeItemStyle={
-        backgroundColor: colors.color1,
-        color: "white"
+        border: `1px solid ${colors.color1}`
+    
     }
     render(){
         return(
-            
-            <div style={{display: "flex", flexDirection: "column"}}>
-                
-                <NavLink  to="/products/all/new/1" style={this.itemStyle} activeStyle={this.activeItemStyle}>
-                    <h1>All</h1>
-                </NavLink>
-                <NavLink  to="/products/Pc/new/1" style={this.itemStyle} activeStyle={this.activeItemStyle}>
-                    <h1>Pc</h1>
-                </NavLink>
-                <NavLink  to="/products/Console/new/1" style={this.itemStyle} activeStyle={this.activeItemStyle}>
-                    <h1>Console</h1>
-                </NavLink>
-                <NavLink  to="/products/Electronics/new/1" style={this.itemStyle} activeStyle={this.activeItemStyle}>  
-                    <h1>Electronics</h1>
-                </NavLink>
-                <NavLink  to="/products/Appliances/new/1" style={this.itemStyle} activeStyle={this.activeItemStyle} >
-                    <h1>Appliances</h1>
-                </NavLink>
-                
-            </div>
+
+            <ListGroup style={{width: "8%"}}>
+                <ListGroup.Item as={NavLink} to="/products/all/new/1" activeStyle={this.activeItemStyle} style={this.itemStyle}>
+                All
+                </ListGroup.Item>
+                <ListGroup.Item as={NavLink} to="/products/Pc/new/1" activeStyle={this.activeItemStyle} style={this.itemStyle}>
+                Pc
+                </ListGroup.Item>
+                <ListGroup.Item as={NavLink} to="/products/Console/new/1" activeStyle={this.activeItemStyle} style={this.itemStyle}>
+                Console
+                </ListGroup.Item>
+                <ListGroup.Item as={NavLink} to="/products/Electronics/new/1" activeStyle={this.activeItemStyle} style={this.itemStyle}>
+                Electronics
+                </ListGroup.Item>
+                <ListGroup.Item as={NavLink} to="/products/Appliances/new/1" activeStyle={this.activeItemStyle} style={this.itemStyle}>
+                Appliances
+                </ListGroup.Item>
+            </ListGroup>
+                        
             
         )
     }
